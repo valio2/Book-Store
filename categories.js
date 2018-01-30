6670ecb2db1b6e6bd6bbe728e288f64c8eb4f64f
valio2 <Valios2@hotmail.com>
2018-01-30 11:22:02 +0200

    added the 'database'

---
+++ categories.js
@@ -0,0 +1,83 @@
+var categories = {
+    javascript: {
+        book1: {
+            title: "JavaScript for Kids: A Playful Introduction to Programming",
+            author: "Nick Morgan",
+            year: 2014,
+            pic: "https://images-na.ssl-images-amazon.com/images/I/51YfamJFhkL._SX376_BO1,204,203,200_.jpg",
+            pages: 309,
+            description: `JavaScript is the programming language of the Internet,
+            the secret sauce that makes the Web awesome,
+            your favorite sites interactive,
+            and online games fun!`
+        },
+        book2: {
+            title: 'JavaScript: The Good Parts',
+            author: 'Douglas Crockford',
+            year: 2008,
+            pic: 'https://covers.oreillystatic.com/images/9780596517748/lrg.jpg',
+            pages: 172,
+            description: `Most programming languages contain good and bad parts, but JavaScript has more than its share of the bad, having been developed and released in a hurry before it could be refined. This authoritative book scrapes away these bad features to reveal a subset of JavaScript that's more reliable, readable, and maintainable than the language as a whole—a subset you can use to create truly extensible and efficient code.`,
+        },
+        book3: {
+            title: 'Secrets of the JavaScript Ninja, Second Edition',
+            author: 'John Resig, Bear Bibeault, and Josip Maras',
+            year: 2016,
+            pic: 'https://images.manning.com/720/960/resize/book/b/72b6dbb-4eb6-4bbd-9078-b47f0393a1bb/Resig-JSN-2ed-HI.png',
+            pages: 464,
+            description: `More than ever, the web is a universal platform for all types of applications, and JavaScript is the language of the web. If you're serious about web development, it's not enough to be a decent JavaScript coder. You need to be ninja-stealthy, efficient, and ready for anything. This book shows you how.`,
+        },
+    },
+    csharp: {
+        book1: {
+            title: 'Pro C# 5.0 and the .NET 4.5 Framework',
+            author: 'Andrew Troelsen',
+            year: 2012,
+            pic: 'https://images.springer.com/sgw/books/medium/9781430242338.jpg',
+            pages: 1560,
+            description: `This new edition of Pro C# 5.0 and the .NET 4.5 Platform has been completely revised and rewritten to reflect the latest changes to the C# language specification and new advances in the .NET Framework. You'll find new chapters covering the important new features that make .NET 4.5 the most comprehensive release yet.`,
+        },
+        book2: {
+            title: 'C# in Depth, Third Edition',
+            author: 'Jon Skeet',
+            year: 2013,
+            pic: 'https://images.manning.com/720/960/resize/book/f/0f9d189-3823-4078-ba3d-423f23172bd8/skeet3.png',
+            pages: 616,
+            description: 'C# in Depth, Third Edition updates the best-selling second edition to cover the new features of C# 5, including the challenges of writing maintainable asynchronous code. It preserves the uniquely insightful look into the tricky areas and dusty corners of C# that only expert Jon Skeet can provide.',
+        },
+        book3: {
+            title: 'Adaptive Code via C#: Class and Interface Design, Design Patterns, and SOLID Principles',
+            author: 'Gary McLean Hall',
+            year: 2014,
+            pic: 'https://images-na.ssl-images-amazon.com/images/I/51I13xOnnGL._SX408_BO1,204,203,200_.jpg',
+            pages: 406,
+            description: 'Your process may be agile, but are you building agility directly into the code base? This book teaches .NET programmers how to give code the flexibility to adapt to changing requirements and customer demands by applying cutting-edge techniques, including SOLID principles, design patterns, and other industry best practices.',
+        },
+    },
+    java: {
+        book1: {
+            title: 'Head First Java, 2nd Edition',
+            author: 'Kathy Sierra',
+            year: 2005,
+            pic: 'https://images-na.ssl-images-amazon.com/images/I/51Gsycdh-TL._SX430_BO1,204,203,200_.jpg',
+            pages: 688,
+            description: `Learning a complex new language is no easy task especially when it s an object-oriented computer programming language like Java. You might think the problem is your brain. It seems to have a mind of its own, a mind that doesn't always want to take in the dry, technical stuff you're forced to study.`,
+        },
+        book2: {
+            title: `Java: A Beginner's Guide, Sixth Edition: A Beginner's Guide, Sixth Edition`,
+            author: 'Herbert Schildt',
+            year: 2014,
+            pic: 'https://images.gr-assets.com/books/1397424275l/17349359.jpg',
+            pages: 728,
+            description: `Essential Java Programming Skills--Made Easy! Fully updated for Java Platform, Standard Edition 8 (Java SE 8), "Java: A Beginner's Guide," Sixth Edition gets you started programming in Java right away. Bestselling programming author Herb Schildt begins with the basics, such as how to create, compile, and run a Java program. He then moves on to the keywords, syntax, and constructs that form the core of the Java language. This Oracle Press resource also covers some of Java's more advanced features, including multithreaded programming, generics, and Swing. Of course, new Java SE 8 features such as lambda expressions and default interface methods are described. An introduction to JavaFX, Java's newest GUI, concludes this step-by-step tutorial.`,
+        },
+        book3: {
+            title: 'Java Performance: The Definitive Guide',
+            author: 'Scott Oaks',
+            year: 2014,
+            pic: 'https://covers.oreillystatic.com/images/0636920028499/cat.gif',
+            pages: 426,
+            description: `Coding and testing are often considered separate areas of expertise. In this comprehensive guide, author and Java expert Scott Oaks takes the approach that anyone who works with Java should be equally adept at understanding how code behaves in the JVM, as well as the tunings likely to help its performance.`,
+        },
+    },
+}
