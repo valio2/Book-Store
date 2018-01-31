6527e55cb9bf745d7541a8a26baf1f7c19ee9493
Georgi Moskov <georgi.m.moskov@gmail.com>
2018-01-31 14:31:51 +0200

    improved code quality and readibility of script.js and style.css

--- script.js
+++ script.js
@@ -4,12 +4,12 @@
 
     for (category in categories) {
         var curUl = $('<ul class="category"></ul>');
-        var curP = $('<p class="categTitle categ-icon"></p>').text(category);
+        var curP = $('<p class="categ-text"></p>').text(category);
         curUl.append($('<i class="fa fa-angle-double-right book-icon" aria-hidden="true"></i>'));
         curUl.append(curP);
 
         var curLiDiv = $('<div class="liDiv"></div>');
-        curLiDiv.css('display','none');
+        curLiDiv.css('display', 'none');
 
         for (book in categories[category]) {
             var curTitle = book;
@@ -21,59 +21,48 @@
 
     }
     $('.nav').append(navTempDiv);
-})();   
-
+})();
 
 //show and hide categories in left navigation
-/*$(".categTitle, .book-icon").on("click", function () {
-    $(event.target).siblings(':last').slideToggle(250);
-})
-*/
 
-$(".category").on("click", function(){
-    var evTarget = $(event.target);
-    if(evTarget.is('ul')){
-        evTarget.children('.liDiv').slideToggle(250);
+$(".category").on("click", function (event) {
+    var evThis = $(this);
 
-        evTarget.children(':first').toggleClass('fa-angle-double-right');
-        evTarget.children(':first').toggleClass('fa-angle-double-down');
-
-        
-    }else if(evTarget.is('.categ-icon') || evTarget.is('.book-icon')){
-        evTarget.siblings(':last').slideToggle(250);
-        evTarget.parent().children(':first').toggleClass('fa-angle-double-right');
-        evTarget.parent().children(':first').toggleClass('fa-angle-double-down');
+    if (evThis.is('ul')) {
+        event.stopPropagation();
+        evThis.children('.liDiv').slideToggle(250);
+        evThis.children('.book-icon').toggleClass('fa-angle-double-right');
+        evThis.children('.book-icon').toggleClass('fa-angle-double-down');
     }
-
-
 })
 
 
 //visualize book page
 $(".category div li").on("click", function (event) {
-    $('.content').fadeOut(300, function () {
-        $(this).html('');
-        var target = $(event.target);
+    var evThis = $(this);
+    if (evThis.is('.category div li')) {
+        event.stopPropagation();
+        $('.content').fadeOut(300, function () {
+            $('.content').html('');
+            var category = evThis.parent().siblings('.categ-text').text().trim();
+            var title = evThis.text().trim();
+            var author = categories[category][title]['author'];
+            var year = categories[category][title]['year'];
+            var pic = categories[category][title]['pic'];
+            var pages = categories[category][title]['pages'];
+            var description = categories[category][title]['description'];
 
-        var category = target.parent().prev().html().trim();
-        var title = target.html().trim();
+            var innerHTML = `<h1>${title}</h1>
+                <p class="author">${author}</p>
+                <img src="${pic}" class="book-pic">
+                <p>Year: ${year}</p>
+                <p>${pages}</p>
+                <p>${description}</p>`
 
+            $('.content').html(innerHTML).fadeIn(800);
+        })
+    }
 
-        var author = categories[category][title]['author'];
-        var year = categories[category][title]['year'];
-        var pic = categories[category][title]['pic'];
-        var pages = categories[category][title]['pages'];
-        var description = categories[category][title]['description'];
-
-        var innerHTML = `<h1>${title}</h1>
-            <p class="author">${author}</p>
-            <img src="${pic}" class="book-pic">
-            <p>Year: ${year}</p>
-            <p>${pages}</p>
-            <p>${description}</p>`
-
-        $(this).html(innerHTML).fadeIn(800);
-    })
 
 
 })
