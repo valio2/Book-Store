74ef41e4eb1b59006e65dbbee46e8fca3da5fb00
valio2 <Valios2@hotmail.com>
2018-01-31 18:45:04 +0200

    created working home-page and home-page buttons

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
         curUl.append(curP);
         var spanCurBookCount = $('<span></span>');
 
-        var countBooksInCateg = (function retunrsNumberOfBooksInCategory () {
+        var countBooksInCateg = (function retunrsNumberOfBooksInCategory() {
             var countBooks = 0;
             for (book in categories[category]) {
                 if (categories[category].hasOwnProperty(book)) {
@@ -39,16 +39,37 @@
     $('.nav').append(navTempDiv);
 })();
 
+
+
 // build home page
 
+function buildHomePage() {
+    var homePageContent = $('<div class="home-page-wrapper"></div>');
+    for (category in categories) {
+        var curThumbnailDiv = $('<div class="thumbnails-category"></div>');
+        curThumbnailDiv.append($(`<h2></h2>`).text(category));
 
+        for (book in categories[category]) {
+            var curBookDiv = $('<div></div>');
+            curBookDiv.append($(`<img src="${categories[category][book]['pic']}" class="book-pic">`));
+            curBookDiv.append($(`<h6>${book}</h6>`));
 
+            curThumbnailDiv.append(curBookDiv);
+        }
+        homePageContent.append(curThumbnailDiv);
+    }
+    $('.content').append(homePageContent);
+};
+buildHomePage();
 
-
-
-
-
-
+// make the home button to build the home page
+$(".nav-home, .logo-box").on("click", function () {
+    $('.content').fadeOut(300, function() {
+        $('.content').html('');
+        buildHomePage();
+        $('.content').fadeIn(1000, buildHomePage);
+    })
+});
 
 
 //show and hide categories in left navigation
@@ -59,8 +80,8 @@ $(".category").on("click", function (event) {
     if (evThis.is('ul')) {
         event.stopPropagation();
         evThis.children('.liDiv').slideToggle(250);
-        evThis.children('.book-icon').toggleClass('fa-angle-double-right');
-        evThis.children('.book-icon').toggleClass('fa-angle-double-down');
+        evThis.children('.categ-icon').toggleClass('fa-angle-double-right');
+        evThis.children('.categ-icon').toggleClass('fa-angle-double-down');
     }
 })
 
@@ -92,7 +113,4 @@ $(".category div li").on("click", function (event) {
             $('.content').html(innerHTML).fadeIn(800);
         })
     }
-
-
-
 })
